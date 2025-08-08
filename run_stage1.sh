388ffd559be0be6b9f2865f24ec5e44b60719927
moonlight03 <44736355+moonlight03@users.noreply.github.com>
2025-08-08 10:27:29 +0800

    Update run_stage1.sh

--- run_stage1.sh
+++ run_stage1.sh
@@ -5,7 +5,7 @@ do
      # python train.py -s ./data/${name} -m output_${name}/${name}_stage1 --src_type monocap --train_stage 1 --epochs 200 --pose_op_start_iter 10 --end_epoch 200 --unseen_view 0
      python render_novel_pose.py -s ./data/${name} -m output_${name}/${name}_stage1 --src_type monocap --epoch 200 --pose observ --stage 1
      python render_novel_pose.py -s ./data/${name} -m output_${name}/${name}_stage1 --src_type monocap --epoch 200 --pose tpose --stage 1
-     python render_novel_pose1.py -s data/${name} -m output_${name}/${name}_stage1 --epoch 300
+     python render_novel_pose1.py -s data/${name} -m output_${name}/${name}_stage1 --epoch 200
      python eval.py -s ./data/${name} -m output_${name}/${name}_stage1 --train_stage 1 --epoch 200
 
 
